76d768ea783ed595149510771a71a4f486ae88ff
Quazi Yousuf <sayera1900@gmail.com>
2026-04-07 01:34:44 +0600

    second

--- src/components/pwa/PushProvider.tsx
+++ src/components/pwa/PushProvider.tsx
@@ -65,7 +65,7 @@ export function PushProvider({ userId, children }: Props) {
           // 4. Subscribe with VAPID key
           subscription = await registration.pushManager.subscribe({
             userVisibleOnly: true,
-            applicationServerKey: urlBase64ToUint8Array(VAPID_PUBLIC_KEY),
+            applicationServerKey: urlBase64ToUint8Array(VAPID_PUBLIC_KEY) as unknown as BufferSource,
           });
         }
 
